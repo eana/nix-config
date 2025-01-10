7531947dbfe40f3958fbf56e4e1f1e082e7d4c02
Jonas Eana <jonas@eana.ro>
2025-01-11 00:00:46 +0100

    Add neovim config

---
+++ .config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,21 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "bash-language-server",
+      "gopls",
+      "json-lsp",
+      "jsonlint",
+      "jsonnet-language-server",
+      "lua-language-server",
+      "prettierd",
+      "shellcheck",
+      "shfmt",
+      "stylua",
+      "terraform-ls",
+      "tflint",
+      "yaml-language-server",
+      "yamlfmt",
+    },
+  },
+}
