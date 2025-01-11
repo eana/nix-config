786908c56500f3c94bc794a92c351eb1708126f4
Jonas Eana <jonas@eana.ro>
2025-01-11 19:33:49 +0100

    Add the nvidia drivers

--- flake.nix
+++ flake.nix
@@ -27,8 +27,9 @@
     };
   };
 
-  # nixConfig = {
-  #   extra-substituters = "https://cuda-maintainers.cachix.org";
-  #   extra-trusted-public-keys = "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=";
-  # };
+  nixConfig = {
+    extra-substituters = "https://cuda-maintainers.cachix.org";
+    extra-trusted-public-keys =
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=";
+  };
 }
