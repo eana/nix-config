f3ff350ab192f00fe3f2bbf8021bef4c440d6c61
Jonas Eana <jonas@eana.ro>
2025-01-16 18:02:28 +0100

    docs: describe how to get nvidiaBusId and intelBusId values

--- system.nix
+++ system.nix
@@ -22,6 +22,9 @@ in {
     graphics.enable = true;
     nvidia = {
       prime = {
+        # ❯ nix-shell -p pciutils --run "lspci -nn | grep -E 'VGA|3D'"
+        # 00:02.0 VGA compatible controller [0300]: Intel Corporation HD Graphics 530 [8086:191b] (rev 06)
+        # 01:00.0 3D controller [0302]: NVIDIA Corporation GM107GLM [Quadro M1000M] [10de:13b1] (rev a2)
         nvidiaBusId = "PCI:1:0:0";
         intelBusId = "PCI:0:2:0";
       };
