b659756cdd6841a0d1d322c5b239c350ee9c1356
Jonas Eana <jonas@eana.ro>
2025-01-18 22:31:47 +0100

    refactor: move static config files to assets and modularize Nix configurations

---
+++ base/systemd.nix
@@ -0,0 +1,61 @@
+{ pkgs, ... }:
+
+{
+  startServices = "sd-switch";
+
+  services = {
+    copyq = {
+      Unit = {
+        Description = "CopyQ clipboard management daemon";
+        PartOf = [ "graphical-session.target" ];
+        After = [ "graphical-session.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.copyq}";
+        Restart = "on-failure";
+        Environment = [ "QT_QPA_PLATFORM=xcb" ];
+      };
+      Install.WantedBy = [ "sway-session.target" ];
+    };
+
+    telegram = {
+      Unit = {
+        Description = "Telegram Desktop";
+        PartOf = [ "graphical-session.target" ];
+        After = [ "graphical-session.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.telegram-desktop} -startintray";
+        Restart = "on-failure";
+        Environment = [ "QT_QPA_PLATFORM=xcb" ];
+      };
+      Install.WantedBy = [ "sway-session.target" ];
+    };
+
+    bluetooth-applet = {
+      Unit = {
+        Description = "Blueman Applet";
+        PartOf = [ "graphical-session.target" ];
+        After = [ "graphical-session.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.blueman}/bin/blueman-applet";
+        Restart = "on-failure";
+      };
+      Install.WantedBy = [ "sway-session.target" ];
+    };
+
+    swaynag-battery = {
+      Unit = {
+        Description = "Low battery notification";
+        PartOf = [ "graphical-session.target" ];
+        After = [ "graphical-session.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.swaynag-battery}/bin/swaynag-battery";
+        Restart = "on-failure";
+      };
+      Install.WantedBy = [ "sway-session.target" ];
+    };
+  };
+}
