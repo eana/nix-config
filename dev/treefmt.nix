0f5d58a04afa3278bd57de4fb1a68455582a71fe
Jonas Eana <jonas@eana.ro>
2025-02-20 19:30:54 +0100

    feat: use terlar/dev-flake

---
+++ dev/treefmt.nix
@@ -0,0 +1,21 @@
+{ pkgs }:
+{
+  programs = {
+    nixfmt = {
+      enable = true;
+      package = pkgs.nixfmt-rfc-style;
+    };
+    stylua = {
+      enable = true;
+      package = pkgs.stylua;
+      settings = {
+        indent_type = "Spaces";
+        indent_width = 2;
+      };
+    };
+    mdformat = {
+      enable = true;
+      package = pkgs.mdformat;
+    };
+  };
+}
