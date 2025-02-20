acd4c4928813ecaa4f76a3b91f728140d217f280
Jonas Eana <jonas@eana.ro>
2025-02-20 21:19:36 +0100

    Update and run the pre-commit hooks

--- dev/pre-commit.nix
+++ dev/pre-commit.nix
@@ -1,6 +1,8 @@
 {
   settings.hooks = {
-    end-of-file-fixer.enable = true;
+    check-json.enable = true;
+    check-xml.enable = true;
     conform.enable = false;
+    end-of-file-fixer.enable = true;
   };
 }
