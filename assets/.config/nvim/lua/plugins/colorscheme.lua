486d89d9e9038341a37acef01be4a47f27fa93f6
Jonas Eana <jonas@eana.ro>
2025-03-03 14:59:18 +0100

    Toggle between dark and light themes

--- assets/.config/nvim/lua/plugins/colorscheme.lua
+++ assets/.config/nvim/lua/plugins/colorscheme.lua
@@ -15,6 +15,20 @@ return {
       vim.opt.termguicolors = true
       vim.opt.background = "dark"
       vim.cmd("colorscheme hybrid_reverse")
+
+      -- Define a function to toggle between dark and light themes
+      local function toggle_theme()
+        if vim.opt.background:get() == "dark" then
+          vim.opt.background = "light"
+          vim.cmd("colorscheme hybrid_material") -- Use the light variant of the theme
+        else
+          vim.opt.background = "dark"
+          vim.cmd("colorscheme hybrid_reverse") -- Use the dark variant of the theme
+        end
+      end
+
+      -- Map a keybinding to toggle the theme (e.g., <Leader>tt)
+      vim.keymap.set("n", "<Leader>tt", toggle_theme, { desc = "Toggle between dark and light themes" })
     end,
   },
 }
