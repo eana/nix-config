a78463297d9f6743c0d44b9a861212189f259ce8
Jonas Eana <jonas@eana.ro>
2025-04-23 18:34:24 +0200

    feat: enable nvidia driver

--- system/services.nix
+++ system/services.nix
@@ -38,11 +38,7 @@
       };
     };
     desktopManager.gnome.enable = true;
-    videoDrivers = [ ];
-    # Disable the nvidia driver and use the intel one instead
-    # [
-    #   "nvidia"
-    # ];
+    videoDrivers = [ "nvidia" ];
   };
 
   printing = {
@@ -55,19 +51,4 @@
   openssh.enable = true;
 
   logind.lidSwitchExternalPower = "ignore";
-
-  # Disable the nvidia driver and use the intel one instead
-  udev.extraRules = ''
-    # Remove NVIDIA USB xHCI Host Controller devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-
-    # Remove NVIDIA USB Type-C UCSI devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-
-    # Remove NVIDIA Audio devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-
-    # Remove NVIDIA VGA/3D controller devices
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-  '';
 }
