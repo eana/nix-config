d0c113eac58827ed0f5828fccaf64fabd694018b
Jonas Eana <jonas@eana.ro>
2025-05-01 22:34:04 +0200

    feat: extend OpenRA module to install RA, Dune2k, and CNC

--- modules/openra.nix
+++ modules/openra.nix
@@ -8,8 +8,31 @@ with lib;
 
 let
   cfg = config.module.openra;
-  openra-pkg =
-    pkgs.runCommand "openra"
+
+  variants = {
+    red-alert = {
+      name = "Red Alert";
+      appimageSuffix = "Red-Alert";
+      modDir = "ra";
+    };
+    dune = {
+      name = "Dune 2000";
+      appimageSuffix = "Dune-2000";
+      modDir = "d2k";
+    };
+    tiberian-dawn = {
+      name = "Tiberian Dawn";
+      appimageSuffix = "Tiberian-Dawn";
+      modDir = "cnc";
+    };
+  };
+
+  mkOpenRAPackage =
+    variant:
+    let
+      variantCfg = variants.${variant};
+    in
+    pkgs.runCommand "openra-${variant}"
       {
         buildInputs = [
           pkgs.makeWrapper
@@ -21,15 +44,15 @@ let
           zlib
         ];
         src = pkgs.fetchurl {
-          url = "https://github.com/OpenRA/OpenRA/releases/download/${cfg.release}/OpenRA-Red-Alert-x86_64.AppImage";
-          sha256 = cfg.appimageSha256;
+          url = "https://github.com/OpenRA/OpenRA/releases/download/${cfg.release}/OpenRA-${variantCfg.appimageSuffix}-x86_64.AppImage";
+          sha256 = cfg.variants.${variant}.appimageSha256;
         };
       }
       ''
         mkdir -p $out/bin
-        cp $src $out/bin/openra.AppImage
-        chmod +x $out/bin/openra.AppImage
-        makeWrapper ${pkgs.appimage-run}/bin/appimage-run $out/bin/openra \
+        cp $src $out/bin/openra-${variant}.AppImage
+        chmod +x $out/bin/openra-${variant}.AppImage
+        makeWrapper ${pkgs.appimage-run}/bin/appimage-run $out/bin/openra-${variant} \
           --prefix LD_LIBRARY_PATH : "${
             pkgs.lib.makeLibraryPath (
               with pkgs;
@@ -41,69 +64,86 @@ let
               ]
             )
           }" \
-          --add-flags "$out/bin/openra.AppImage"
+          --add-flags "$out/bin/openra-${variant}.AppImage"
       '';
 
-  desktopItem = pkgs.makeDesktopItem {
-    name = "openra";
-    exec = "${openra-pkg}/bin/openra";
-    icon = "openra";
-    comment = "Open Source reimplementation of Red Alert";
-    desktopName = "OpenRA Red Alert";
-    genericName = "Strategy Game";
-    categories = [
-      "Game"
-      "StrategyGame"
-    ];
-    terminal = false;
-    startupWMClass = "openra";
-  };
+  mkDesktopItem =
+    variant:
+    let
+      variantCfg = variants.${variant};
+    in
+    pkgs.makeDesktopItem {
+      name = "openra-${variant}";
+      exec = "${mkOpenRAPackage variant}/bin/openra-${variant}";
+      icon = "openra-${variant}";
+      comment = "Open Source reimplementation of ${variantCfg.name}";
+      desktopName = "OpenRA ${variantCfg.name}";
+      genericName = "Strategy Game";
+      categories = [
+        "Game"
+        "StrategyGame"
+      ];
+      terminal = false;
+      startupWMClass = "openra";
+    };
 
-  openra-icon = pkgs.fetchurl {
-    url = cfg.iconUrl;
-    sha256 = cfg.iconSha256;
-  };
+  mkIcon =
+    variant:
+    pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/OpenRA/OpenRA/refs/tags/${cfg.release}/mods/${variants.${variant}.modDir}/icon.png";
+      sha256 = cfg.variants.${variant}.iconSha256;
+    };
+
+  enabledVariants = lib.attrsets.filterAttrs (name: _: cfg.variants.${name}.enable or false) variants;
 
 in
 {
   options.module.openra = {
     enable = mkEnableOption "Open Source real-time strategy game engine";
+
     release = mkOption {
       type = types.str;
       default = "release-20250330";
       description = "OpenRA release version";
     };
-    appimageSha256 = mkOption {
-      type = types.str;
-      default = "sha256-PLccdCgYjIUm3YkWmT/Bb6F7pfKKNZaKgmfz258hhv4=";
-      description = "SHA256 hash of the OpenRA AppImage";
-    };
-    iconSha256 = mkOption {
-      type = types.str;
-      default = "sha256-6IadsH5NGKXZ3gye3JYVTCDC/uPwy3BRXhuAp5+10qA=";
-      description = "SHA256 hash of the OpenRA icon";
-    };
-    iconUrl = mkOption {
-      type = types.str;
-      default = "https://raw.githubusercontent.com/OpenRA/OpenRA/refs/tags/${cfg.release}/mods/ra/icon.png";
-      description = "URL to fetch OpenRA icon from";
+
+    variants = mkOption {
+      type = types.attrsOf (
+        types.submodule {
+          options = {
+            enable = mkEnableOption "Enable this OpenRA variant";
+            appimageSha256 = mkOption {
+              type = types.str;
+              description = "SHA256 hash of the OpenRA AppImage for this variant";
+            };
+            iconSha256 = mkOption {
+              type = types.str;
+              description = "SHA256 hash of the OpenRA icon for this variant";
+            };
+          };
+        }
+      );
+      default = { };
+      description = "Configuration for each OpenRA variant";
     };
   };
 
   config = mkIf cfg.enable {
-    home.packages = [
-      openra-pkg
-      (pkgs.runCommand "openra-desktop"
-        {
-          nativeBuildInputs = [ pkgs.copyDesktopItems ];
-          desktopItems = [ desktopItem ];
-        }
-        ''
-          mkdir -p $out/share/icons/hicolor/64x64/apps
-          cp ${openra-icon} $out/share/icons/hicolor/64x64/apps/openra.png
-          copyDesktopItems
-        ''
-      )
-    ];
+    home.packages = lib.lists.concatLists (
+      lib.attrsets.mapAttrsToList (variant: _: [
+        (mkOpenRAPackage variant)
+        (pkgs.runCommand "openra-${variant}-desktop"
+          {
+            nativeBuildInputs = [ pkgs.copyDesktopItems ];
+            desktopItems = [ (mkDesktopItem variant) ];
+          }
+          ''
+            mkdir -p $out/share/icons/hicolor/64x64/apps
+            cp ${mkIcon variant} $out/share/icons/hicolor/64x64/apps/openra-${variant}.png
+            copyDesktopItems
+          ''
+        )
+      ]) enabledVariants
+    );
   };
 }
