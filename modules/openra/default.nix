a8353ab77a2db6e4844396bc2c9d255f0844d527
Jonas Eana <jonas@eana.ro>
2025-05-23 08:38:40 +0200

    refactor: mhalo and OpenRA modules into subdirectories
    - Move mhalo and OpenRA modules to their own directories with default.nix and package.nix files
    - Split OpenRA desktop integration into a separate file
    - Update flake.nix to use new module paths

---
+++ modules/openra/default.nix
@@ -0,0 +1,103 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with lib;
+
+let
+  cfg = config.module.openra;
+
+  variants = {
+    red-alert = {
+      name = "Red Alert";
+      appimageSuffix = "Red-Alert";
+      modDir = "ra";
+    };
+    dune = {
+      name = "Dune 2000";
+      appimageSuffix = "Dune-2000";
+      modDir = "d2k";
+    };
+    tiberian-dawn = {
+      name = "Tiberian Dawn";
+      appimageSuffix = "Tiberian-Dawn";
+      modDir = "cnc";
+    };
+  };
+
+  enabledVariants = filterAttrs (name: _: cfg.variants.${name}.enable or false) variants;
+
+  mkVariant =
+    variant: variantCfg:
+    let
+      desktop = pkgs.callPackage ./desktop.nix {
+        inherit
+          lib
+          pkgs
+          variant
+          variantCfg
+          cfg
+          ;
+      };
+    in
+    [
+      (pkgs.callPackage ./package.nix {
+        inherit
+          lib
+          pkgs
+          variant
+          variantCfg
+          cfg
+          ;
+      })
+      (pkgs.runCommand "openra-${variant}-desktop"
+        {
+          nativeBuildInputs = [ pkgs.copyDesktopItems ];
+          desktopItems = [ desktop.desktopItem ];
+        }
+        ''
+          mkdir -p $out/share/icons/hicolor/64x64/apps
+          cp ${desktop.icon} $out/share/icons/hicolor/64x64/apps/openra-${variant}.png
+          copyDesktopItems
+        ''
+      )
+    ];
+
+in
+{
+  options.module.openra = {
+    enable = mkEnableOption "Open Source real-time strategy game engine";
+
+    release = mkOption {
+      type = types.str;
+      default = "release-20250330";
+      description = "OpenRA release version";
+    };
+
+    variants = mkOption {
+      type = types.attrsOf (
+        types.submodule {
+          options = {
+            enable = mkEnableOption "Enable this OpenRA variant";
+            appimageSha256 = mkOption {
+              type = types.str;
+              description = "SHA256 hash of the OpenRA AppImage for this variant";
+            };
+            iconSha256 = mkOption {
+              type = types.str;
+              description = "SHA256 hash of the OpenRA icon for this variant";
+            };
+          };
+        }
+      );
+      default = { };
+      description = "Configuration for each OpenRA variant";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = concatLists (mapAttrsToList mkVariant enabledVariants);
+  };
+}
