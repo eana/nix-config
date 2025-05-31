47f8590a5402eeb3367f5672ebf5d2e52a36047b
Jonas Eana <jonas@eana.ro>
2025-05-31 22:16:41 +0200

    Add CopilotToggle and CopilotGitMessage commands to Copilot

--- assets/.config/nvim/lua/plugins/copilot.lua
+++ assets/.config/nvim/lua/plugins/copilot.lua
@@ -9,6 +9,34 @@ return {
     opts = {
       -- See Configuration section for options
     },
-    -- See Commands section for default commands if you want to lazy load on them
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot", -- Optional lazy-loading
+    config = function()
+      require("copilot").setup({
+        -- You can add your Copilot config options here
+      })
+
+      local copilot_enabled = true
+      vim.api.nvim_create_user_command("CopilotToggle", function()
+        if copilot_enabled then
+          vim.cmd("Copilot disable")
+          print("Copilot disabled")
+        else
+          vim.cmd("Copilot enable")
+          print("Copilot enabled")
+        end
+        copilot_enabled = not copilot_enabled
+      end, {})
+
+      vim.api.nvim_create_user_command("CopilotGitMessage", function()
+        vim.cmd("normal! GVgg")
+        vim.cmd("CopilotChat Please write a git commit message for these changes")
+      end, {})
+
+      vim.keymap.set("n", "<leader>ct", ":CopilotToggle<CR>", { desc = "Toggle Copilot" })
+      vim.keymap.set("n", "<leader>cg", ":CopilotGitMessage<CR>", { desc = "Copilot generate git commit message" })
+    end,
   },
 }
