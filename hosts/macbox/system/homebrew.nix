95f44a945611ae6a65a9ebe39cd82b88540c7929
Jonas Eana <jonas@eana.ro>
2025-06-24 14:58:28 +0200

    refactor: macos host configuration into modular files
    - Split hosts/macbox/default.nix into smaller modules:
      - system/defaults.nix: macOS defaults and preferences
      - system/environment.nix: environment variables and activation scripts
      - system/homebrew.nix: Homebrew and nix-homebrew setup
      - system/packages.nix: system packages
      - system/security.nix: security and Nix settings
      - home-manager/default.nix: user and home-manager configuration
      - nix/default.nix: Nix and nixpkgs settings
    - Update hosts/macbox/default.nix to import new modules
    - Remove direnv from home/users/jonas/common.nix
    - No functional changes, improves maintainability and clarity

---
+++ hosts/macbox/system/homebrew.nix
@@ -0,0 +1,34 @@
+{ config, inputs, ... }:
+let
+  inherit (inputs) nix-homebrew;
+  inherit (inputs) homebrew-core;
+  inherit (inputs) homebrew-cask;
+in
+{
+  homebrew = {
+    enable = true;
+    casks = [
+      "altserver"
+      "iterm2"
+      "openvpn-connect"
+      "podman-desktop"
+      "vlc"
+    ];
+    # masApps = {
+    #   Bitwarden = 1352778147;
+    # };
+    onActivation.cleanup = "zap";
+    taps = builtins.attrNames config.nix-homebrew.taps;
+  };
+
+  nix-homebrew = {
+    enable = true;
+    user = "jonas";
+
+    taps = {
+      "homebrew/homebrew-core" = homebrew-core;
+      "homebrew/homebrew-cask" = homebrew-cask;
+    };
+    mutableTaps = false;
+  };
+}
