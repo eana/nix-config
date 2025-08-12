9efd949ea0460125334ea6552b115d59be6d61eb
Jonas Eana <jonas@eana.ro>
2025-08-12 15:20:00 +0200

    feat: update cachix substituters and trusted public keys

--- flake.nix
+++ flake.nix
@@ -132,13 +132,7 @@
     };
 
   nixConfig = {
-    extra-substituters = [
-      "https://cuda-maintainers.cachix.org"
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+    extra-substituters = "https://eana.cachix.org https://cuda-maintainers.cachix.org";
+    extra-trusted-public-keys = "eana.cachix.org-1:3sJHATrL9zjGFGZwAXpECSMR+Ql5k02GgdxfJyzHi84= cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=";
   };
 }
