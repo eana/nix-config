6f220c5f5cc2118a02d7b4b1bdc630dd717659a5
Jonas Eana <jonas@eana.ro>
2025-08-21 22:56:49 +0200

    feat(nasbox): add host config with nix and home-manager modules
    - Add hosts/nasbox/default.nix to define imports and timezone
    - Add hosts/nasbox/nix/default.nix with Nix settings:
      - Configure substituters and trusted-public-keys
      - Set trusted-users and enable experimental features (nix-command, flakes)
      - Enable automatic garbage collection
      - Allow unfree packages

---
+++ hosts/nasbox/nix/default.nix
@@ -0,0 +1,32 @@
+{ config, ... }:
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://cache.nixos.org"
+        "https://cuda-maintainers.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      ];
+
+      trusted-users = [
+        "root"
+      ];
+
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+
+    gc = {
+      automatic = true;
+      dates = "12:12";
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+}
