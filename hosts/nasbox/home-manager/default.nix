9380fde207b3004c891428834bc0b7622f22563b
Jonas Eana <jonas@eana.ro>
2025-08-22 14:18:14 +0200

    refactor(nasbox): move root home-manager config to dedicated file
    - Extract root user's Home Manager configuration into `home/users/root/ds920p.nix`
    - Update `hosts/nasbox/home-manager/default.nix` to import the new config and set up `home-manager.users.root`
    - Enable `programs.nix-index-database.comma` for improved nix-index integration

--- hosts/nasbox/home-manager/default.nix
+++ hosts/nasbox/home-manager/default.nix
@@ -1,33 +1,13 @@
-{ pkgs, ... }:
-
 {
-  imports = [
-    ../../../modules/common/default.nix
-  ];
-
-  home = {
-    username = "root";
-    homeDirectory = "/root";
-    stateVersion = "24.05";
-
-    packages = with pkgs; [
-      nix-tree # Visualize Nix dependencies
-    ];
-  };
-
-  programs = {
-    direnv = {
-      enable = true;
-      nix-direnv.enable = true;
+  home-manager = {
+    users.root = {
+      imports = [
+        ../../../home/users/root/ds920p.nix
+        ../../../modules/common/default.nix
+      ];
+      home.stateVersion = "24.05";
     };
-    home-manager.enable = true;
   };
 
-  module = {
-    git.enable = true;
-    gpg-agent.enable = true;
-    neovim.enable = true;
-    tmux.enable = true;
-    zsh.enable = true;
-  };
+  programs.nix-index-database.comma.enable = true;
 }
