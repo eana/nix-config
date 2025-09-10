72e7bcee3a2172a022c1030446c650ee845f5dcf
Jonas Eana <jonas@eana.ro>
2025-09-10 22:12:34 +0200

    nix: update substituters and trusted-public-keys configuration
    - Add `eana.cachix.org` substituter and its public key
    - Remove per-host `substituters` and `trusted-public-keys` settings from macbox, nasbox, and nixbox configs to centralize configuration

--- hosts/nasbox/nix/default.nix
+++ hosts/nasbox/nix/default.nix
@@ -4,15 +4,6 @@
   nix = {
     package = pkgs.nix;
     settings = {
-      substituters = [
-        "https://cache.nixos.org"
-        "https://cuda-maintainers.cachix.org"
-      ];
-
-      trusted-public-keys = [
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      ];
-
       trusted-users = [
         "root"
       ];
