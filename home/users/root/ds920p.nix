6dab6b81cde5c3cf283cd7de82652a40ff0a3ea5
Jonas Eana <jonas@eana.ro>
2025-09-17 09:24:19 +0200

    feat: add network and system utilities to ds920p.nix package list

--- home/users/root/ds920p.nix
+++ home/users/root/ds920p.nix
@@ -17,9 +17,21 @@
   # Search for packages here: https://search.nixos.org/packages
   home = {
     packages = with pkgs; [
+      dnsutils # DNS utilities like dig, nslookup, etc.
+      file # File type identification utility
+      iftop # Display bandwidth usage on an interface
+      inetutils # Collection of common network programs
+      iotop # Display I/O usage by processes
+      iperf # Network bandwidth measurement tool
+      iputils # Network utilities like ping, traceroute, etc.
+      lsof # List open files
       ncurses5 # Terminal handling library
+      nethogs # Display bandwidth usage per process
       nix-tree # Visualize Nix dependencies
+      procps # Utilities for monitoring system processes
+      sysstat # Performance monitoring tools
       util-linux # Set of system utilities for Linux
+      zmap # Network scanner
     ];
 
     sessionVariables = {
