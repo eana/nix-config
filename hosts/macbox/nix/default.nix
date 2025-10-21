e3da7a3fe2b1ca95fdf8419c6f54adf8572d2de8
Jonas Eana <jonas@eana.ro>
2025-10-21 09:29:03 +0200

    chore(nix): disable upstream nix service and settings on macbox
    - Fix dyld symbol not found error with upstream nix on macOS
    - Switch to determinate nix for compatibility
    - See: https://github.com/nixos/nix/issues/14201

--- hosts/macbox/nix/default.nix
+++ hosts/macbox/nix/default.nix
@@ -1,36 +1,5 @@
 {
-  nix = {
-    settings = {
-      substituters = [
-        "https://cache.nixos.org"
-        "https://eana.cachix.org"
-        "https://cuda-maintainers.cachix.org"
-        "https://nix-community.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:viD+eA0uK1Hgf7UqfE1u+GE7xRbLPaY9I+aa8pUkiNw="
-        "eana.cachix.org-1:3sJHATrL9zjGFGZwAXpECSMR+Ql5k02GgdxfJyzHi84="
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-
-      trusted-users = [
-        "root"
-        "jonas"
-      ];
-
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-    };
-
-    gc = {
-      automatic = true;
-      interval.Hour = 3;
-      options = "--delete-older-than 14d";
-    };
-  };
+  nix.enable = false;
 
   nixpkgs = {
     hostPlatform = "x86_64-darwin";
