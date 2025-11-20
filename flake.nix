8bc2208d3342ae9b64749078a4ca81ab65c025c7
Jonas Eana <jonas@eana.ro>
2025-11-20 22:28:36 +0100

    fix(nix): update cache.nixos.org public key in configs

--- flake.nix
+++ flake.nix
@@ -156,6 +156,6 @@
 
   nixConfig = {
     extra-substituters = "https://cache.nixos.org https://eana.cachix.org https://cuda-maintainers.cachix.org https://nix-community.cachix.org";
-    extra-trusted-public-keys = "cache.nixos.org-1:viD+eA0uK1Hgf7UqfE1u+GE7xRbLPaY9I+aa8pUkiNw= eana.cachix.org-1:3sJHATrL9zjGFGZwAXpECSMR+Ql5k02GgdxfJyzHi84= cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+    extra-trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= eana.cachix.org-1:3sJHATrL9zjGFGZwAXpECSMR+Ql5k02GgdxfJyzHi84= cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
   };
 }
--- hosts/nixbox/nix/default.nix
+++ hosts/nixbox/nix/default.nix
@@ -8,7 +8,7 @@
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
-        "cache.nixos.org-1:viD+eA0uK1Hgf7UqfE1u+GE7xRbLPaY9I+aa8pUkiNw="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "eana.cachix.org-1:3sJHATrL9zjGFGZwAXpECSMR+Ql5k02GgdxfJyzHi84="
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
