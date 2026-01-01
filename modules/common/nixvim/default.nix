603ae63ed58af3fd630e854a73d54f49393380cd
Jonas Eana <jonas@eana.ro>
2026-01-01 22:55:36 +0100

    feat(nixvim): add highlight on yank autocmd

--- modules/common/nixvim/default.nix
+++ modules/common/nixvim/default.nix
@@ -103,6 +103,25 @@ in
       extraPlugins = with pkgs.vimPlugins; [
         undotree
       ];
+
+      # Highlight on yanking
+      autoGroups = {
+        highlight-yank = {
+          clear = true;
+        };
+      };
+      autoCmd = [
+        {
+          event = [ "TextYankPost" ];
+          desc = "Highlight when yanking (copying) text";
+          group = "highlight-yank";
+          callback.__raw = ''
+            function()
+              vim.highlight.on_yank()
+            end
+          '';
+        }
+      ];
     };
   };
 }
