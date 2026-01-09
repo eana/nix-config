564c482c66783958dfe9dae02749e34c86fb8393
Jonas Eana <jonas@eana.ro>
2026-01-09 09:41:44 +0100

    feat(nixvim): switch colorscheme to gruvbox-material
    - Enable gruvbox-material colorscheme
    - Remove hybrid-material and related configuration
    - Update theme toggle command for the light theme

--- modules/common/nixvim/plugins/colorscheme.nix
+++ modules/common/nixvim/plugins/colorscheme.nix
@@ -1,34 +1,27 @@
-{ pkgs, ... }:
+_:
+
 {
   programs.nixvim = {
-    extraPlugins = with pkgs.vimPlugins; [
-      vim-hybrid-material
-    ];
-
-    opts = {
-      termguicolors = true;
-      background = "dark";
-    };
-
-    globals = {
-      enable_bold_font = 1;
+    colorschemes.gruvbox-material = {
+      enable = true;
+      settings = {
+        enable_bold = 1;
+        enable_italic = 1;
+        foreground = "material";
+        spell_foreground = "colored";
+        material_cursor = "green";
+        material_better_performance = 1;
+      };
     };
 
     extraConfigLua = ''
-      -- Enable true color
-      vim.env.NVIM_TUI_ENABLE_TRUE_COLOR = 1
+      vim.cmd("colorscheme gruvbox-material")
 
-      -- Set initial colorscheme
-      vim.cmd("colorscheme hybrid_reverse")
-
-      -- Toggle between dark and light variants
       vim.api.nvim_create_user_command("ToggleTheme", function()
         if vim.opt.background:get() == "dark" then
           vim.opt.background = "light"
-          vim.cmd("colorscheme hybrid_material")
         else
           vim.opt.background = "dark"
-          vim.cmd("colorscheme hybrid_reverse")
         end
       end, {})
     '';
