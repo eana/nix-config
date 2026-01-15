4cc92562edf9b522c6931580b7ecd5df713db084
Jonas Eana <jonas@eana.ro>
2026-01-15 18:20:29 +0100

    feat(nixvim/plugins): split mini, which-key, gitsigns configs
    - Move mini, which-key, and gitsigns plugin configs to separate files
    - Update imports in default.nix to use new plugin config files

--- modules/common/nixvim/plugins/default.nix
+++ modules/common/nixvim/plugins/default.nix
@@ -1,16 +1,21 @@
 {
   imports = [
+    # keep-sorted start
     ./blink-cmp.nix
     ./bufferline.nix
     ./conform-nvim.nix
     ./copilot.nix
     ./d2.nix
+    ./gitsigns.nix
     ./lint.nix
     ./lsp.nix
     ./lualine.nix
+    ./mini.nix
     ./snacks-nvim.nix
     ./themery-nvim.nix
     ./treesitter.nix
+    ./which-key.nix
+    # keep-sorted end
   ];
 
   programs.nixvim = {
@@ -25,117 +30,8 @@
           scope.enabled = false;
         };
       };
-      mini = {
-        enable = true;
-        mockDevIcons = true;
-        modules = {
-          icons = { };
-          indentscope = {
-            symbol = "│";
-            draw = {
-              delay = 0;
-              animation.__raw = "require('mini.indentscope').gen_animation.none()";
-            };
-          };
-          surround = {
-            mappings = {
-              add = "gsa";
-              delete = "gsd";
-              find = "gsf";
-              find_left = "gsF";
-              highlight = "gsh";
-              replace = "gsr";
-              update_n_lines = "gsn";
-            };
-          };
-        };
-      };
       trim.enable = true;
       web-devicons.enable = true;
-      which-key = {
-        enable = true;
-        settings = {
-          spec = [
-            {
-              __unkeyed-1 = "<leader><tab>";
-              group = "tabs";
-            }
-            {
-              __unkeyed-1 = "<leader>c";
-              group = "code";
-            }
-            {
-              __unkeyed-1 = "<leader>d";
-              group = "debug";
-            }
-            {
-              __unkeyed-1 = "<leader>dp";
-              group = "profiler";
-            }
-            {
-              __unkeyed-1 = "<leader>f";
-              group = "file/find";
-            }
-            {
-              __unkeyed-1 = "<leader>g";
-              group = "git";
-            }
-            {
-              __unkeyed-1 = "<leader>gh";
-              group = "hunks";
-            }
-            {
-              __unkeyed-1 = "<leader>q";
-              group = "quit/session";
-            }
-            {
-              __unkeyed-1 = "<leader>s";
-              group = "search";
-            }
-            {
-              __unkeyed-1 = "<leader>u";
-              group = "ui";
-            }
-            {
-              __unkeyed-1 = "<leader>x";
-              group = "diagnostics/quickfix";
-            }
-            {
-              __unkeyed-1 = "[";
-              group = "prev";
-            }
-            {
-              __unkeyed-1 = "]";
-              group = "next";
-            }
-            {
-              __unkeyed-1 = "g";
-              group = "goto";
-            }
-            {
-              __unkeyed-1 = "gs";
-              group = "surround";
-            }
-            {
-              __unkeyed-1 = "z";
-              group = "fold";
-            }
-            {
-              __unkeyed-1 = "<leader>b";
-              group = "buffer";
-            }
-            {
-              __unkeyed-1 = "<leader>w";
-              group = "windows";
-              proxy = "<C-w>";
-            }
-            {
-              __unkeyed-1 = "gx";
-              desc = "Open with system app";
-            }
-          ];
-        };
-      };
 
       # --- Editing Productivity ---
       nvim-autopairs.enable = true;
@@ -151,37 +47,6 @@
 
       # --- Git Integration ---
       diffview.enable = true;
-      gitsigns = {
-        enable = true;
-        settings = {
-          signs = {
-            add.text = "▎";
-            change.text = "▎";
-            delete.text = "";
-            topdelete.text = "";
-            changedelete.text = "▎";
-            untracked.text = "▎";
-          };
-          signs_staged = {
-            add.text = "▎";
-            change.text = "▎";
-            delete.text = "";
-            topdelete.text = "";
-            changedelete.text = "▎";
-          };
-        };
-        luaConfig.post = ''
-          require("snacks").toggle({
-            name = "Git Signs",
-            get = function()
-              return require("gitsigns.config").config.signcolumn
-            end,
-            set = function(state)
-              require("gitsigns").toggle_signs(state)
-            end,
-          }):map("<leader>uG")
-        '';
-      };
 
       # --- Diagnostics & Troubleshooting ---
       trouble.enable = true;
