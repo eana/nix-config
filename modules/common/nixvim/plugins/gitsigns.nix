ded54432c201921a44bff373be7c4ed446b135f1
Jonas Eana <jonas@eana.ro>
2026-01-15 18:20:29 +0100

    feat(nixvim/plugins): split mini, which-key, gitsigns configs
    - Move mini, which-key, and gitsigns plugin configs to separate files
    - Update imports in default.nix to use new plugin config files

---
+++ modules/common/nixvim/plugins/gitsigns.nix
@@ -0,0 +1,35 @@
+_:
+
+{
+  programs.nixvim.plugins.gitsigns = {
+    enable = true;
+    settings = {
+      signs = {
+        add.text = "▎";
+        change.text = "▎";
+        delete.text = "";
+        topdelete.text = "";
+        changedelete.text = "▎";
+        untracked.text = "▎";
+      };
+      signs_staged = {
+        add.text = "▎";
+        change.text = "▎";
+        delete.text = "";
+        topdelete.text = "";
+        changedelete.text = "▎";
+      };
+    };
+    luaConfig.post = ''
+      require("snacks").toggle({
+        name = "Git Signs",
+        get = function()
+          return require("gitsigns.config").config.signcolumn
+        end,
+        set = function(state)
+          require("gitsigns").toggle_signs(state)
+        end,
+      }):map("<leader>uG")
+    '';
+  };
+}
