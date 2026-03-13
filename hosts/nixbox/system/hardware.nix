1a4dedfcef7b2a57cee8029bfb775ba5ce2dadb4
Jonas Eana <jonas@eana.ro>
2026-03-13 14:56:18 +0100

    feat(system): replace neofetch with fastfetch and remove sleep overrides
    - Replace neofetch with fastfetch in user and root configs
    - Remove systemd sleep extraConfig from nixbox hardware config

--- hosts/nixbox/system/hardware.nix
+++ hosts/nixbox/system/hardware.nix
@@ -6,11 +6,4 @@
     };
     graphics.enable = true;
   };
-
-  systemd.sleep.extraConfig = ''
-    AllowSuspend=no
-    AllowHibernation=no
-    AllowHybridSleep=no
-    AllowSuspendThenHibernate=no
-  '';
 }
