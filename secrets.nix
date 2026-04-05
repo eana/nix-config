6ca2a21a52b2e12a54b82397745793d18868d2c2
Jonas Eana <jonas@eana.ro>
2026-04-05 11:10:24 +0200

    fix: update ssh user and host keys
    - Update macbox ssh host key
    - Re-encrypt secrets/ssh-hosts.age with new keys

--- secrets.nix
+++ secrets.nix
@@ -4,13 +4,13 @@
 # age --recipients-file <(grep -o 'ssh-ed25519 [A-Za-z0-9+/=]\+' secrets.nix) -o secrets/file.age file.txt
 let
   # ssh user keys
-  jonas-macbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIhkZXZS9kncETCALGhU4mKrooISoYsaSrMPYBf4ss7v";
+  jonas-macbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJqDynIVkdPDrRame489hgimATFFya3mqyCaKdQ3B0Ek";
   jonas-nasbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeDavVuEljfsfBMW90Gy///+pERcQP20++fAFbFksdx";
   jonas-nixbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE+qpCo4eLhdNH32FORP21FzrDizHK7krsaFU3Naxxtn";
   jonas-oldbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKQbkQjVrARK5CI74PvEYXI3UCicE0nwiMYShOl4P31e";
 
   # ssh host keys
-  macbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILiXjg7W4r7OFBQ0Av7rysETgvJNZdcaxWXtQ5gfHq7r";
+  macbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIxphbCkRRfxIDVjt5usEvX7tIyBLqMHJQGadghYpzww";
   nasbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICc5dgjlWWmoXG4gBYDy2mozfWSIErmtKVAgRP6XpAgs";
   nixbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWvSi5GNjd3TqP0hX1tqZmEFgtCDJnPZ5ymXFe0k9lP";
   oldbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOYdM304AQ1hLve1bnEFf139KlvI9cVKrOPGNibG1A4O";
