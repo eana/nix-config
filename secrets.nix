622460ef8b084ad895df632c177f0dc88ad6873a
Jonas Eana <jonas@eana.ro>
2026-09-21 23:53:29 +0200

    chore(secrets): add newbox recipient keys and re-encrypt

--- secrets.nix
+++ secrets.nix
@@ -6,24 +6,28 @@ let
   # ssh user keys
   jonas-macbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJqDynIVkdPDrRame489hgimATFFya3mqyCaKdQ3B0Ek";
   jonas-nasbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeDavVuEljfsfBMW90Gy///+pERcQP20++fAFbFksdx";
+  jonas-newbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBAQ9gbh0jn/zYrRRuNpA7YwTtobLOqzh7bqc4tMJO7Y";
   jonas-nixbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE+qpCo4eLhdNH32FORP21FzrDizHK7krsaFU3Naxxtn";
   jonas-oldbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKQbkQjVrARK5CI74PvEYXI3UCicE0nwiMYShOl4P31e";
 
   # ssh host keys
   macbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIxphbCkRRfxIDVjt5usEvX7tIyBLqMHJQGadghYpzww";
   nasbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICc5dgjlWWmoXG4gBYDy2mozfWSIErmtKVAgRP6XpAgs";
+  newbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILdbAFJkJvIOfYoDECc1DgHCqOqCQyYD95LNqX08PfSV";
   nixbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWvSi5GNjd3TqP0hX1tqZmEFgtCDJnPZ5ymXFe0k9lP";
   oldbox = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOYdM304AQ1hLve1bnEFf139KlvI9cVKrOPGNibG1A4O";
 
   allSystems = [
     macbox
     nasbox
+    newbox
     nixbox
     oldbox
   ];
   allUsers = [
     jonas-macbox
     jonas-nasbox
+    jonas-newbox
     jonas-nixbox
     jonas-oldbox
   ];
