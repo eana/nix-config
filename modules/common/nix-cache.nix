e8f1db0ac837730dce6b83f50fee961e78e0ee4e
Jonas Eana <jonas@eana.ro>
2026-09-22 02:10:24 +0200

    fix(nix-cache): remove decommissioned cuda cache
    - Drop cuda-maintainers.cachix.org substituter and public key from flake nixConfig and shared nix-cache module
    - Cache moved to nix-community.cachix.org in Nov 2025, already configured

--- modules/common/nix-cache.nix
+++ modules/common/nix-cache.nix
@@ -5,7 +5,6 @@ _: {
     substituters = [
       # keep-sorted start
       "https://cache.nixos.org"
-      "https://cuda-maintainers.cachix.org"
       "https://eana.cachix.org"
       "https://nix-community.cachix.org"
       # keep-sorted end
@@ -13,7 +12,6 @@ _: {
     trusted-public-keys = [
       # keep-sorted start
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "eana.cachix.org-1:3sJHATrL9zjGFGZwAXpECSMR+Ql5k02GgdxfJyzHi84="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       # keep-sorted end
